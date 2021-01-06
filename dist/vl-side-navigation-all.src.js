763660721bc240b811879e618e6a3e838c65d482
Tom Coemans <tom.coemans@gmail.com>
2021-01-06 10:24:03 +0100

    initial code

---
+++ dist/vl-side-navigation-all.src.js
@@ -0,0 +1,18 @@
+import {VlSideNavigation} from 'vl-ui-side-navigation/dist/vl-side-navigation.src.js';
+import {VlSideNavigationReference} from 'vl-ui-side-navigation/dist/vl-side-navigation-reference.src.js';
+import {VlSideNavigationTitle} from 'vl-ui-side-navigation/dist/vl-side-navigation-title.src.js';
+import {VlSideNavigationContent} from 'vl-ui-side-navigation/dist/vl-side-navigation-content.src.js';
+import {VlSideNavigationGroup} from 'vl-ui-side-navigation/dist/vl-side-navigation-group.src.js';
+import {VlSideNavigationItem} from 'vl-ui-side-navigation/dist/vl-side-navigation-item.src.js';
+import {VlSideNavigationToggle} from 'vl-ui-side-navigation/dist/vl-side-navigation-toggle.src.js';
+
+export {
+  VlSideNavigation,
+  VlSideNavigationReference,
+  VlSideNavigationTitle,
+  VlSideNavigationContent,
+  VlSideNavigationGroup,
+  VlSideNavigationItem,
+  VlSideNavigationToggle,
+};
+
