abfe9f112df49a1b23c6722fd8c7a6148e6af253
Tom Coemans <tom.coemans@gmail.com>
2021-01-29 12:46:27 +0100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-import {VlSideNavigation} from '../dist/vl-side-navigation.src.js';
-import {VlSideNavigationReference} from '../dist/vl-side-navigation-reference.src.js';
-import {VlSideNavigationTitle} from '../dist/vl-side-navigation-title.src.js';
-import {VlSideNavigationContent} from '../dist/vl-side-navigation-content.src.js';
-import {VlSideNavigationGroup} from '../dist/vl-side-navigation-group.src.js';
-import {VlSideNavigationItem} from '../dist/vl-side-navigation-item.src.js';
-import {VlSideNavigationToggle} from '../dist/vl-side-navigation-toggle.src.js';
-
-export {
-  VlSideNavigation,
-  VlSideNavigationReference,
-  VlSideNavigationTitle,
-  VlSideNavigationContent,
-  VlSideNavigationGroup,
-  VlSideNavigationItem,
-  VlSideNavigationToggle,
-};
+export * from '../dist/vl-side-navigation-all.src.js';
